518cd46a9278063c92e1c8e2147ffdf6ed42b555
moinulhossainmahim <moinulhossainmahim@gmail.com>
2021-07-02 00:25:02 +0600

    added all functionalities

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        dignissimos neque enim vero, eveniet accusantium amet cum atque
+        repudiandae repellat quo explicabo laboriosam veniam at ea ullam
+        deleniti vitae, accusamus expedita molestias sed sapiente! Voluptatum et
+        velit dolore libero fugit?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
